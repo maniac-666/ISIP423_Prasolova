de135b9f97ea68d44189aab87f10e2d398a6da32
maniac-666 <ekaterinaang@icloud.com>
2025-10-14 11:24:25 +0300

    Инициализация библиотеки и главное меню.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var library = new List<Book>
+            {
+                new Book("Война и Мир", "Кто воевал и мрился", "Любовь", 1869, 500),
+                new Book("Преступление и наказание", "Федор Достоевский", "Дед и зайцы", 1866, 450),
+                new Book("Робинзон крузо", "Михаил ", "Золушка", 1967, 600),
+                new Book("Три мушкетера", "Александр", "Малахитовая шкатулкак", 1844, 300),
+                new Book("Отцы и дети", "Иван Тургеев", "Доктор кто", 1862, 400)
+            };
+        
+    
+
+            
