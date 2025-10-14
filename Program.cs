4c843e8700878b9b9d0b0c99eb3a72d6e10982c3
maniac-666 <ekaterinaang@icloud.com>
2025-10-14 11:26:11 +0300

    меню с использованием свитч кейса.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,57 @@ namespace LibraryApp
                 new Book("Три мушкетера", "Александр", "Малахитовая шкатулкак", 1844, 300),
                 new Book("Отцы и дети", "Иван Тургеев", "Доктор кто", 1862, 400)
             };
-        
-    
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1 - Добавить книгу");
+                Console.WriteLine("2 - Удалить книгу по Id");
+                Console.WriteLine("3 - Найти книги");
+                Console.WriteLine("4 - Отсортировать книги по названию");
+                Console.WriteLine("5 - Отсортировать книги по году");
+                Console.WriteLine("6 - Вывести самую дорогую и самую дешёвую книгу");
+                Console.WriteLine("7 - Сгруппировать книги по авторам");
+                Console.WriteLine("0 - Выход");
+                Console.Write("Выберите команду: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int cmd)) continue;
+                if (cmd == 0) break;
+
+                switch (cmd)
+                {
+                    case 1:
+                        AddBook(library);
+                        break;
+                    case 2:
+                        RemoveBook(library);
+                        break;
+                    case 3:
+                        SearchBooks(library);
+                        break;
+                    case 4:
+                        var byTitle = library.OrderBy(b => b.Title);
+                        PrintBooks(byTitle);
+                        break;
+                    case 5:
+                        var byYear = library.OrderBy(b => b.Year);
+                        PrintBooks(byYear);
+                        break;
+                    case 6:
+                        var max = library.OrderByDescending(b => b.Price).First();
+                        var min = library.OrderBy(b => b.Price).First();
+                        Console.WriteLine($"Дорогая: {max}");
+                        Console.WriteLine($"Дешёвая: {min}");
+                        break;
+                    case 7:
+                        var grouped = library.GroupBy(b => b.Author);
+                        foreach (var g in grouped)
+                            Console.WriteLine($"{g.Key} -> {g.Count()} книг");
+                        break;
+                }
+            }
+        }
+
+
+
 
             
