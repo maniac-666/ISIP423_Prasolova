4a9418d521567c8cb7a0fccd3ffc549c18dc1a69
maniac-666 <ekaterinaang@icloud.com>
2025-10-28 11:12:16 +0300

    Класс сущностей.

---
+++ Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP423_Prasolova.Entities
+{
+    internal abstract class Entity
+    {
+        public int Hp;
+        public int Damage;
+        public abstract void GetDamage(int damage);
+    }
+}
